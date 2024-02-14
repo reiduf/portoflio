6b5758625354257e7efd9010c7fe11fa9fbe7e73
reina-duf <reina.dufrene@gmail.com>
2024-02-13 17:48:52 -0700

    changes theme to print

--- src/js/base.js
+++ src/js/base.js
@@ -1,13 +1,11 @@
 const burger = document.getElementById('burger')
 const navbar = document.getElementById('nav')
-const liEls = document.querySelectorAll('#nav > li')
+
 
 burger.addEventListener('click', function() {
   navbar.classList.toggle('hidden');
 })
 
 navbar.addEventListener('click', function(evt) {
-  if (evt.target.tagName === 'UL') return;
-  liEls.forEach(li => li.classList.remove('active'));
-  evt.target.classList.add('active')
+  navbar.classList.toggle('hidden');
 })
